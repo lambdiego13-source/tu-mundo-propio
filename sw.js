4bdb0204535da3169fe1456807c77447a8504dbd
lambdiego13-source <lambdiego13@gmail.com>
2026-03-22 19:25:05 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,27 @@
-self.addEventListener('fetch', function(event) {
-  // Esto permite que la app funcione aunque sea un buscador básico
+const cacheName = 'tu-mundo-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icono.png'
+];
+
+// Instalar el Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Responder con archivos en caché
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
