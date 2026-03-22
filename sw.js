bb6b958db34228db502e26936430cb44be124c38
lambdiego13-source <lambdiego13@gmail.com>
2026-03-22 19:48:50 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,8 @@
-const cacheName = 'tu-mundo-v1';
-const assets = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json',
-  './icono.png'
-];
-
-// Instalar el Service Worker
+const cacheName = 'tu-mundo-v2';
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+  self.skipWaiting(); // Esto obliga a la nueva versión a activarse ya
 });
 
-// Responder con archivos en caché
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
